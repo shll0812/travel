a1d732154b49c2b7317b32124a56300d4d93ca6b
shihuanlu <1183225634@qq.com>
2019-07-19 14:54:28 +0800

    'shouye'

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -4,6 +4,7 @@
    <home-swiper></home-swiper>
    <home-icons></home-icons>
    <home-recommend></home-recommend>
+   <home-weekend></home-weekend>
   </div>  
 </template>
 <script>
@@ -11,13 +12,15 @@ import HomeHeader from './components/header.vue'
 import HomeSwiper from './components/swiper.vue'
 import HomeIcons from './components/icons.vue'
 import HomeRecommend from './components/recommend.vue'
+import HomeWeekend from './components/weekend.vue'
 export default {
   name: 'Home',
   components:{
       HomeHeader,
       HomeSwiper,
       HomeIcons,
-      HomeRecommend
+      HomeRecommend,
+      HomeWeekend
   }
 }
 </script>
