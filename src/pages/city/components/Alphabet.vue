d0cd71b2132b9c7a2e63d770379b39e96a37f155
shihuanlu <1183225634@qq.com>
2019-07-20 09:37:11 +0800

    '8-4'

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul class="list">
+        <li class="item">A</li>
+        <li class="item">B</li>
+        <li class="item">A</li>
+        <li class="item">B</li>
+        <li class="item">A</li>
+        <li class="item">B</li>
+        <li class="item">A</li>
+        <li class="item">B</li>
+        <li class="item">A</li>
+        <li class="item">B</li>
+    </ul>
+</template>
+
+<script>
+export default{
+    name:"CityAlphabet"
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.list
+    display:flex
+    flex-direction:column
+    justify-content:center
+    position:absolute
+    right:0
+    top:1.58rem
+    bottom:0
+    width:.4rem
+    .item
+        line-heigth:.4rem
+        text-align:center
+        color:$bgColor
+</style>
